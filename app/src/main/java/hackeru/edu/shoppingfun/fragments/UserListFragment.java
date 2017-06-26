85ba6190d02ff69043e3e320a46153618cc98c58
hackeru <hackerudroids@gmail.com>
2017-06-26 20:09:27 +0300

    Added User Lists

--- app/src/main/java/hackeru/edu/shoppingfun/fragments/UserListFragment.java
+++ app/src/main/java/hackeru/edu/shoppingfun/fragments/UserListFragment.java
@@ -5,15 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,7 +15,6 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import hackeru.edu.shoppingfun.R;
 import hackeru.edu.shoppingfun.dialogs.AddListDialogFragment;
-import hackeru.edu.shoppingfun.models.UserList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,26 +45,6 @@ public class UserListFragment extends Fragment {
 
     @OnClick(R.id.fabAddUserList)
     public void onFabAddUserClicked() {
-        //1) ref the user - > userID
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser == null) {
-            Log.e("Tomer", "No User!");
-            return; //No User-> No DB...{}
-        }
-
-        //2) listID = ref UserListTable->UID->push()
-
-        DatabaseReference newUserListRowRef = FirebaseDatabase.getInstance().
-                getReference("UserLists").
-                child(currentUser.getUid()).
-                push();
-        //UserLists/8b6XnHvv07N5j4Gsw6AZHpgZ4kw2/-GKljfsdkljfdskljfs
-
-        //3) create a new UserList Model
-        UserList list = new UserList();
-        //4) ref.setValue(userList)
-
-
         AddListDialogFragment dialog = new AddListDialogFragment();
         dialog.show(getChildFragmentManager(), "AddUserListDialog");
     }
