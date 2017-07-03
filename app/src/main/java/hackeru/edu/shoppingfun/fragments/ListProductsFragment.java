eaafc7b9be3bf98195ab16b1a272d6f19133d981
hackeru <hackerudroids@gmail.com>
2017-07-03 18:20:39 +0300

    Products fragment and product model

--- app/src/main/java/hackeru/edu/shoppingfun/fragments/ListProductsFragment.java
+++ app/src/main/java/hackeru/edu/shoppingfun/fragments/ListProductsFragment.java
@@ -2,29 +2,65 @@ package hackeru.edu.shoppingfun.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import hackeru.edu.shoppingfun.R;
+import hackeru.edu.shoppingfun.models.UserList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListProductsFragment extends Fragment {
+    @BindView(R.id.rvProducts)
+    RecyclerView rvProducts;
+    @BindView(R.id.fabAddProduct)
+    FloatingActionButton fabAddProduct;
+    @BindView(R.id.etProductName)
+    EditText etProductName;
 
+    Unbinder unbinder;
+    //properties:
+    private UserList userList;
 
-    public ListProductsFragment() {
-        // Required empty public constructor
+    public static ListProductsFragment newInstance(UserList userList) {
+        Bundle args = new Bundle();
+        args.putParcelable("list", userList);
+        ListProductsFragment fragment = new ListProductsFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        userList = getArguments().getParcelable("list");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_products, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_products, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.fabAddProduct)
+    public void onAddProductClicked() {
+        //get the product name from the edit text:
+        String productName = etProductName.getText().toString();
+
+    }
 }
